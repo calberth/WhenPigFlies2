e60f8f1e81dd5696294a91e2dd7150d264520f6e
Casey Albert-Hall <case19@att.net>
2017-05-30 21:50:26 -0700

    Garlic Bombs V2

--- js/garlic.js
+++ js/garlic.js
@@ -1,9 +1,10 @@
 
 
 function dropGarlic() {
-    var garlic = garlics.create(player.x, player.y, 'garlic');
+    var garlic = garlics.create(player.x, player.y + 20, 'garlic');
     game.physics.arcade.enable(garlic);
     garlic.enableBody = true;
     garlic.body.gravity.y = 200;
     garlic.body.bounce.y = 1;
+    garlic.body.velocity.x = player.body.velocity.x;
 }
