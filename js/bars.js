2b0f26009b0061d48657ad942fe6df4008c3b7a5
Alexa Drenick <alexa.drenick@gmail.com>
2017-05-30 22:26:28 -0700

    modularization and stamina bar bitches

---
+++ js/bars.js
@@ -0,0 +1,44 @@
+var healthBar;
+var staminaBar;
+var healthDec = 10;
+var healthText;
+var staminaText;
+
+
+
+function createHealthBar()
+{
+    var bar = game.add.bitmapData(health, 40);
+    bar.ctx.beginPath();
+    bar.ctx.rect(0,0,180,30);
+    bar.ctx.fillStyle = '#00685e';
+    bar.ctx.fill();
+
+    healthBar = game.add.sprite(150,16, bar);
+    healthText = game.add.text(16, 16, 'Health:', { fontSize: '32px', fill: '#000' });
+}
+
+function updateHealthBar()
+{
+    healthBar.width = health;
+}
+
+function createStaminaBar()
+{
+    var bar = game.add.bitmapData(stamina, 40);
+    bar.ctx.beginPath();
+    bar.ctx.rect(0,0,180,30);
+    bar.ctx.fillStyle = '#ff6600';
+    bar.ctx.fill();
+
+    staminaBar = game.add.sprite(150,50, bar);
+    staminaText = game.add.text(16, 50, 'Stamina:', { fontSize: '32px', fill: '#000' });
+}
+
+function updateStaminaBar()
+{
+    staminaBar.width = stamina;
+}
+
+
+    
