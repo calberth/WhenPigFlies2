e91eac9063f1d578b6c91bfa4d52464a3b71682f
Casey Albert-Hall <checkoutuser@pcp137134pcs.wireless.calpoly.edu>
2017-06-11 11:49:50 -0700

    flash on hurt,  condensed overlaps into hitEnemy

--- js/level.js
+++ js/level.js
@@ -72,10 +72,10 @@ function checkCollisions()
     game.physics.arcade.overlap(garlics, bats, collectBat, null, this);
 
     // checks soundwave collision with player
-    game.physics.arcade.overlap(player, soundwaves, collectWave, null, this);
-    game.physics.arcade.overlap(player, bats, hitBat, null, this);
+    game.physics.arcade.overlap(player, soundwaves, hitEnemy, null, this);
+    game.physics.arcade.overlap(player, bats, hitEnemy, null, this);
 
-    game.physics.arcade.overlap(player, diveBats, hitDiveBat, null, this); //Divebat and bat are difference groups
+    game.physics.arcade.overlap(player, diveBats, hitEnemy, null, this); //Divebat and bat are difference groups
     game.physics.arcade.overlap(garlics, diveBats, killDBat, null, this);
 
     // check boundaries and advance player if necessary
@@ -87,7 +87,7 @@ function checkCollisions()
     //game.physics.arcade.overlap(player, garlics, collectGarlic, null, this);
     game.physics.arcade.overlap(player, sack, collectSack, null, this);
 
-    game.physics.arcade.overlap(player, lightning, collectLightning, null, this);
+    game.physics.arcade.overlap(player, lightning, hitEnemy, null, this);
 
     //updateGGBar();
 }
@@ -168,13 +168,7 @@ function collectBat(garlics, bat)
     bats.remove(bat);
 }
 
-function collectWave(player, soundwaves)
-{
-    if (health > 0)
-    {
-        health -= 0.5;
-    }
-}
+
 
 function collectGarlic(player, garlic) {
     //hasGarlic = true;
@@ -187,22 +181,7 @@ function collectSack(player, sack) {
     hasGarlic = true;
 }
 
-function collectLightning(player, lightning) {
-    if (health > 0) {
-        health -= 0.5;
-    }
+function hitEnemy(player, enemy) {
+    decreaseHealth(.5);
 }
 
-
-function hitBat(player, bat) {
-    if (health > 0)
-    {
-        health -= 0.5;
-    }
-} 
-function hitDiveBat(player, bat) {
-    if (health > 0)
-    {
-        health -= 0.5;
-    }
-}
