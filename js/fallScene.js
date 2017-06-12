37e05eccfdd8922e0b52163c49cb28b3a31a2b0d
Kalea Louie <kalealouie@yahoo.com>
2017-06-11 18:16:54 -0700

    Added falling animation

--- js/fallScene.js
+++ js/fallScene.js
@@ -1,3 +1,7 @@
+var fallFrameCount = 0;
+var saveYVelocity;
+var fallPig;
+
 
 var fallSceneState = {
     create: function() {
@@ -5,7 +9,7 @@ var fallSceneState = {
         initializeLevel(false, false, false, false, false);
         addfallSceneObjects();
 
-        var playButton = this.game.add.button(475, 550, 'play', this.restart, this);
+        // var playButton = this.game.add.button(475, 550, 'play', this.restart, this);
     },
     
     // The restart function calls the menu state    
@@ -30,7 +34,35 @@ var fallSceneState = {
 
     update: function() {
 
+        tumble();
+        // if (player.body.position.y < 500) {
+        //     player.body.position.y += 5;
+        // }
+        // if (fallFrameCount >= 200) {
+        //     var playButton = this.game.add.button(475, 550, 'play', this.restart, this);
+        // }
+
+        fallFrameCount++;
     }
+}
+
+function tumble() {
+    fallPig.angle += 5;
+    //player.body.velocity.y -= 15;
+}
+
+function initPig() {
+    player.body.position.x = game.world.width;
+    player.body.position.y = 0;
+
+    saveYVelocity = player.body.velocity.y;
+
+    fallPig = game.add.sprite(game.world.width / 2, 0, 'pigR1');
+    game.physics.arcade.enable(fallPig);
+    fallPig.enableBody = true;
+    fallPig.anchor.setTo(0.25, 0.5);
+    fallPig.body.velocity.y = 100;
+    // fallPig.angle += 2.5;
 }
 
 function addfallSceneObjects()
@@ -38,4 +70,6 @@ function addfallSceneObjects()
     var scene = background.create(0, 0, 'sky-morning');
 
     var roos = game.add.sprite(-200, 150, 'rooster');
+
+    initPig();
 }
